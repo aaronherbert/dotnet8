f6e790ba0504117332a812fc875c5b73f9616fdc
aaronrac <aaron.herbert@rac.com.au>
2023-11-24 17:47:43 +0800

    (FEATURE): Interceptors

---
+++ Interceptors/Interception.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.CompilerServices;
+ 
+namespace dotnet8.Interceptors
+{
+    public static class Interception
+    {
+        [InterceptsLocation(
+            filePath: @"C:\git\aarongithub\dotnet8\Program.cs",
+            line: 5,
+            character: 9)]
+        public static void InterceptMethod1(
+            this Example example)
+        {
+            Console.WriteLine("Hello from Interceptor");
+        }
+
+        [InterceptsLocation(
+        @"C:\git\aarongithub\dotnet8\Program.cs",
+            7,
+            9)]
+        public static void InterceptMethod2(
+        this Example example, string name)
+        {
+            Console.WriteLine($"Hello from Interceptor, {name}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+var example = new Example();
+
+example.Method1();
+
+example.Method2("name");
+
+public class Example
+{
+    public void Method1()
+    {
+        Console.WriteLine("Hello from Method1");
+    }
+
+    public void Method2(string name)
+    {
+        Console.WriteLine($"Hello from Method2, {name}");
+    }
+}
