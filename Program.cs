869789c2e093f8d6c437a4271e8f38a254499665
aaronrac <aaron.herbert@rac.com.au>
2023-11-24 17:52:14 +0800

    (FEATURE): inline arrays

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//used by runtime team in dotnet
+// https://www.youtube.com/watch?v=Gv2uBJzBAms&t=68s 8:50
+//https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-12.0/inline-arrays
+
+
+var buffer = new InlineBuffer();
+for (int i = 0; i < 10; i++)
+{
+    buffer[i] = i;
+}
+
+foreach (var i in buffer)
+{
+    Console.WriteLine(i);
+}
+
+[System.Runtime.CompilerServices.InlineArray(10)]
+public struct InlineBuffer
+{
+    private int _element0;
+}
