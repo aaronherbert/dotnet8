5d8456b4213d3be0b7af2089215b1718f88fd288
aaronrac <aaron.herbert@rac.com.au>
2023-11-24 17:53:33 +0800

    (FEATURE): ref readonly

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+var example = new Example();
+
+var age = 30;
+
+example.Test(ref age);
+
+Console.WriteLine($"The age is: {age}");
+
+class Example()
+{
+    public void Test(ref readonly int age)
+    {
+        Console.WriteLine($"The age is: {age}");
+        age++;
+    }
+}
