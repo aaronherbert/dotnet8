c0ae2b858ec9e975134017549c02d69e5c9a630c
aaronrac <aaron.herbert@rac.com.au>
2023-11-24 17:45:20 +0800

    (FEATURE): ExperimentalAttr

---
+++ TippicalAaronCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet8
+{
+    [Experimental("Risky")]
+    internal class TypicalAaronCode
+    {
+    }
+}
