feb8284c96a40bc0335f2eed692c83c95a232a8a
aaronrac <aaron.herbert@rac.com.au>
2023-11-24 17:48:56 +0800

    (FEATURE): PrimaryConstructors

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+
+var x = new Person("bob");
+
+x.Hello();
+
+public class Person(string _name)
+{
+    public void Hello()
+    {
+        Console.WriteLine($"Hello {_name}");
+
+        _name = "asd";
+    }
+
+}
+ 
