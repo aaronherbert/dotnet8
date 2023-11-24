47ed5c1e66cfc4cdd9dfe351b73d093addde1d00
aaronrac <aaron.herbert@rac.com.au>
2023-11-24 17:50:45 +0800

    (FEATURE): collection Expressions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int[] a = [1, 2, 3, 4, 5, 6, 7, 8];
+
+Console.WriteLine(a);
+Span<int> b = ['a', 'b', 'c', 'd', 'e', 'f', 'h', 'i'];
+
+int[][] twoD = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+int[] row0 = [1, 2, 3];
+int[] row1 = [4, 5, 6];
+int[] row2 = [7, 8, 9];
+int[] twoDFromVariables = [.. row0, .. row1, .. row2];
+
+Dictionary<string, int> dic = [];
